670c7c970119b847348a9131247bcd692d398f23
precious onanuga <preciousonanuga3@gmail.com>
2025-09-07 23:42:06 +0100

    All at a go! Weewgit add .

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,15 +1,27 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import WelcomeMessage from './WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+  import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+import Counter from './Counter.jsx';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+      <Counter></Counter>
+      <Header />
+      <MainContent />
+      <Footer />
+      <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />       
+       <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
