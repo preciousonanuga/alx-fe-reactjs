190efc0801ad5739845f3a6a04775ca09705d3fb
precious onanuga <preciousonanuga3@gmail.com>
2025-09-28 21:00:35 +0100

    Responsive Typo and Image Sizing

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
   return (
     <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg  ">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto" />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36-h-36 sm:w-24-h-24 mx-auto" />
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
