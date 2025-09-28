36c27e42d0eb60b7192b48740f2da5a32527da69
precious onanuga <preciousonanuga3@gmail.com>
2025-09-28 21:01:42 +0100

    Responsive Typography and Image Sizing

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
   return (
     <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg  ">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36-h-36 sm:w-24-h-24 mx-auto" />
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto" />
       <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
