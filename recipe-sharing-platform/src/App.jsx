a72f6b0d8274cac396bad551973a92d8201323d8
precious onanuga <preciousonanuga3@gmail.com>
2025-10-06 00:05:37 +0100

    adding mock data file

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import HomePage from './components/HomePage'
 import './App.css'
+import HomePage from './components/HomePage'
 
 function App() {
 
   return (
     <>
+      <HomePage />
       <p className="text-red-700">
         Click on the Vite and React logos to learn more
       </p>
